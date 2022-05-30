139557157f2fde501a2ddc35aae8f66a21eda319
Aleksandra <a.haniok@gmail.com>
2022-05-30 12:09:25 +0000

    Import React Bootstrap library to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Moments App!</h1>
+      <Button variant="primary">Hello Button</Button>
     </div>
   );
 }
