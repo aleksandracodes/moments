124f18b5be6c0bd64e25129d44d08383ff6e3fa2
Aleksandra <a.haniok@gmail.com>
2022-05-31 18:45:59 +0000

    Update

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -16,27 +16,22 @@ import {
 } from "react-bootstrap";
 import axios from "axios";
 
-// destructure the useState hook
 const SignUpForm = () => {
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
     password2: "",
   });
-
-  // destructure signUpData to not use the dot notation trying to access their values
   const { username, password1, password2 } = signUpData;
 
-  // useState hook with an  empty object, where we will store all the errors
   const [errors, setErrors] = useState({});
 
   const history = useHistory();
 
   const handleChange = (event) => {
     setSignUpData({
-      // spread the previous signUpData so that we only need to update our relevant attribute
       ...signUpData,
-      [event.target.name]: event.target.value, // create a key: value pair
+      [event.target.name]: event.target.value,
     });
   };
 
@@ -85,7 +80,6 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
-
             {errors.password1?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
@@ -103,7 +97,6 @@ const SignUpForm = () => {
                 onChange={handleChange}
               />
             </Form.Group>
-
             {errors.password2?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
@@ -116,13 +109,11 @@ const SignUpForm = () => {
             >
               Sign up
             </Button>
-
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
             ))}
-
           </Form>
         </Container>
 
