5584654ab1d34434ced7922a6973c22c8ebacaf6
Aleksandra <a.haniok@gmail.com>
2022-06-07 09:27:16 +0000

    Add Profile comp. images,names&(un)follow buttons

---
+++ src/pages/profiles/Profile.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
+
+const Profile = (props) => {
+  const { profile, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+  
+    return (
+        <div
+          className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+        >
+          <div>
+            <Link className="align-self-center" to={`/profiles/${id}`}>
+              <Avatar src={image} height={imageSize} />
+            </Link>
+          </div>
+          <div className={`mx-2 ${styles.WordBreak}`}>
+            <strong>{owner}</strong>
+          </div>
+          <div className={`text-right ${!mobile && "ml-auto"}`}>
+            {/* check if we are on desktop */}
+            {!mobile &&
+            // check if the current user exists so we know that the user is logged in
+              currentUser &&
+              // check if the user is not the owner of the profile because our users won’t be able to follow themselves
+              !is_owner &&
+              (following_id ? (
+                  // in user has followed the profile, then a following_id prop from our API response won’t be null
+                <Button
+                  className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+                  onClick={() => {}}
+                >
+                  unfollow
+                </Button>
+              ) : (
+                <Button
+                  className={`${btnStyles.Button} ${btnStyles.Black}`}
+                  onClick={() => {}}
+                >
+                  follow
+                </Button>
+              ))}
+          </div>
+        </div>
+      );
+    };
+    
+    export default Profile;
