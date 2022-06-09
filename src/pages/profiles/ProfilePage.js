5186a996192006b6e284f112426f5a91c3af171d
Aleksandra <a.haniok@gmail.com>
2022-06-09 07:47:06 +0000

    Add a dropdown menu for users to edit their profile

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -19,6 +19,7 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
@@ -31,7 +32,7 @@ function ProfilePage() {
 
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -52,8 +53,10 @@ function ProfilePage() {
     };
     fetchData();
   }, [id, setProfileData]);
+  
   const mainProfile = (
     <>
+      {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image
